3cca3c6c2246a29c7293669012738b1d22be9f0b
Basabdatta Sen Bhattacharya <basab.sen.b@gmail.com>
2019-03-24 12:06:10 +0530

    Update rk45func_thalmodwithin.m

--- rk45func_thalmodwithin.m
+++ rk45func_thalmodwithin.m
@@ -1,5 +1,6 @@
 function [Y T]=rk45func_thalmodwithin(Carr, initval, Tval, ampaval, gabaAval, gabaBval, leakval, T_ret, Cm)
-%% THIS FUNCTION IMPLEMENTS THE RK45 ALGORITHM FOR THE KINETIC MODEL  BEING WORKED ON BY DAN AND TOM. FEBRUARY 2014.
+%% THIS FUNCTION IMPLEMENTS THE RK45 ALGORITHM FOR THE KINETIC MODEL FOR
+%% THE EXTENDED MODEL BEING WORKED ON BY DAN AND TOM. FEBRUARY 2014.
 % Carr=[Cnte Ctnia Ctnib Ctre Cnsi Cire Ctii Cisi];
 % initval=[Ginit vinit_tcr vinit_trn vinit_in];
 % Tval=[Tmax Kp Vp];
@@ -37,32 +38,34 @@ h=0.001;
 n=gabaBval(8);
 %% RungeKutta45
 for i=1:length(T_ret)
-    r_ampa_ret2tcr=yvect(1,i); %% ampa for the retinal synapse to tcr
-    r_ampa_ret2in=yvect(2,i); %% ampa for the retinal synapse to in
-    r_ampa_tcr2trn=yvect(3,i); %% ampa for the tcr synapse to trn
-    r_gaba_a_trn2tcr=yvect(4,i);%% gabaA synapse 
-    r_gaba_a_trn2trn=yvect(5,i);%% gabaA synapse 
-    r_gaba_a_in2tcr=yvect(6,i);%% gabaA synapse 
-    r_gaba_a_in2in=yvect(7,i);%% gabaA synapse 
-
+r_ampa_ret2tcr=yvect(1,i); %% ampa for the retinal synapse to tcr
+r_ampa_ret2in=yvect(2,i); %% ampa for the retinal synapse to in
+r_ampa_tcr2trn=yvect(3,i); %% ampa for the tcr synapse to trn
+r_gaba_a_trn2tcr=yvect(4,i);%% gabaA synapse 
+r_gaba_a_trn2trn=yvect(5,i);%% gabaA synapse 
+r_gaba_a_in2tcr=yvect(6,i);%% gabaA synapse 
+r_gaba_a_in2in=yvect(7,i);%% gabaA synapse 
+%     r_ampar=yvect(1,i); %% ampa for the retinal synapse 
+%     r_ampat=yvect(2,i); %% ampa for the tcr synapse 
+%     r_gaba_a=yvect(3,i);%% gabaA synapse 
     r_gaba_b=yvect(8,i);%% gabaB synapse 
     G=yvect(9,i); %% The G protein concentration
     V_tcr = yvect(10,i); %% model output: TCR output
     V_trn=yvect(11,i); %% TRN output
     V_in=yvect(12,i); %% IN output
     
-%     % Carr=[Cnte Ctnia Ctnib Ctre Cnsi Cire Ctii Cisi];
-%     % initval=[Ginit vinit_tcr vinit_trn vinit_in];
-%     % Tval=[Tmax Kp Vp];
+% Carr=[Cnte Ctnia Ctnib Ctre Cnsi Cire Ctii Cisi];
+% initval=[Ginit vinit_tcr vinit_trn vinit_in];
+% Tval=[Tmax Kp Vp];
     
-    T_tcr=Tval(1)/(1+exp(-(V_tcr-Tval(3))/Tval(2))); %% transmitter concentration corresponding to the TCR synapse
-    T_trn=Tval(1)/(1+exp(-(V_trn-Tval(3))/Tval(2))); %% transmitter concentration corresponding to the TRN synapse
-    T_in=Tval(1)/(1+exp(-(V_in-Tval(3))/Tval(2))); %% transmitter concentration corresponding to the IN synapse
-%     % ampaval=[E_ampa alpha_ampa beta_ampa g_ampa_tcr2trn g_ampa_ret2tcr g_ampa_ret2in];
-%     % gabaAval=[alpha_gaba_a beta_gaba_a g_gaba_a_trn2tcr g_gaba_a_trn2trn E_gaba_a_trn2tcr E_gaba_a_trn2trn g_gaba_a_in2tcr g_gaba_a_in2in E_gaba_a_in2tcr E_gaba_a_in2in];
-%     % gabaBval=[alpha1_gaba_b beta1_gaba_b alpha2_gaba_b   beta2_gaba_b g_gaba_b E_gaba_b Kd_gaba_b n];
-%     % leakval=[E_leak_tcr g_leak_tcr E_leak_trn g_leak_trn E_leak_in
-%     % g_leak_in];
+    T_tcr=Tval(1)/(1+exp(-(V_tcr-Tval(3))/Tval(2))); %% transmitter concentration of the TCR synapse
+    T_trn=Tval(1)/(1+exp(-(V_trn-Tval(3))/Tval(2))); %% transmitter concentration of the TRN synapse
+    T_in=Tval(1)/(1+exp(-(V_in-Tval(3))/Tval(2))); %% transmitter concentration of the IN synapse
+% ampaval=[E_ampa alpha_ampa beta_ampa g_ampa_tcr2trn g_ampa_ret2tcr g_ampa_ret2in];
+% gabaAval=[alpha_gaba_a beta_gaba_a g_gaba_a_trn2tcr g_gaba_a_trn2trn E_gaba_a_trn2tcr E_gaba_a_trn2trn g_gaba_a_in2tcr g_gaba_a_in2in E_gaba_a_in2tcr E_gaba_a_in2in];
+% gabaBval=[alpha1_gaba_b beta1_gaba_b alpha2_gaba_b   beta2_gaba_b g_gaba_b E_gaba_b Kd_gaba_b n];
+% leakval=[E_leak_tcr g_leak_tcr E_leak_trn g_leak_trn E_leak_in
+% g_leak_in];
 
     I_ampa_ret2tcr =ampaval(5) * r_ampa_ret2tcr * (V_tcr - ampaval(1));
     I_ampa_ret2in =ampaval(6) * r_ampa_ret2in * (V_in - ampaval(1));
@@ -93,7 +96,7 @@ k1_V_trn = h * -(Carr(1)* I_ampa_tcr2trn + Carr(5) * I_gaba_a_trn2trn + I_leak_t
 k1_V_in = h * -(Carr(6)* I_ampa_ret2in + Carr(8) * I_gaba_a_in2in + I_leak_in)*(1/Cm);
 
 
-
+% Carr=[Cnte Ctnia Ctnib Ctre Cnsi Cire Ctii Cisi];
     r_ampa_ret2tcr_temp=r_ampa_ret2tcr+(1/4)*k1_r_ampa_ret2tcr;
     r_ampa_ret2in_temp=r_ampa_ret2in+(1/4)*k1_r_ampa_ret2in;
     r_ampa_tcr2trn_temp=r_ampa_tcr2trn+(1/4)*k1_r_ampa_tcr2trn;
@@ -140,19 +143,26 @@ k2_V_tcr = h * -(Carr(4) * I_ampa_ret2tcr_temp + Carr(2) * I_gaba_a_trn2tcr_temp
 k2_V_trn = h * -(Carr(1) * I_ampa_tcr2trn_temp + Carr(5) * I_gaba_a_trn2trn_temp + I_leak_trn_temp)*(1/Cm);
 k2_V_in =  h * -(Carr(6) * I_ampa_ret2in_temp + Carr(8) * I_gaba_a_in2in_temp + I_leak_in_temp)*(1/Cm);    
  
+% k2_r_ampar = h * (ampaval(2) * T_ret(1,i) * (1-r_ampar_temp) - ampaval(3) * r_ampar_temp);
+% k2_r_ampat = h * (ampaval(2) * T_tcr_temp * (1-r_ampat_temp) - ampaval(3) * r_ampat_temp);
+% k2_r_gaba_a = h * (gabaAval(1) * T_trn_temp * (1-r_gaba_a_temp) - gabaAval(2) * r_gaba_a_temp);
+% k2_r_gaba_b = h * (gabaBval(1) *  T_trn_temp * (1-r_gaba_b_temp) - gabaBval(2)*r_gaba_b_temp);
+% k2_G = h * (gabaBval(3) * r_gaba_b_temp - gabaBval(4) * G_temp);
+% k2_V_tcr = h * -(Carr(4) * I_ampar_temp + Carr(2) * I_gaba_a_trn2tcr_temp + Carr(3) * I_gaba_b_temp + I_leak_tcr_temp)*(1/Cm);
+% k2_V_trn = h * -(Carr(1)* I_ampat_temp + Carr(5) * I_gaba_a_trn2trn_temp + I_leak_trn_temp)*(1/Cm);
 
-    r_ampa_ret2tcr_temp=r_ampa_ret2tcr+(3/32)*k1_r_ampa_ret2tcr+(9/32)*k2_r_ampa_ret2tcr;
-    r_ampa_ret2in_temp=r_ampa_ret2in+(3/32)*k1_r_ampa_ret2in+(9/32)*k2_r_ampa_ret2in;
-    r_ampa_tcr2trn_temp=r_ampa_tcr2trn+(3/32)*k1_r_ampa_tcr2trn+(9/32)*k2_r_ampa_tcr2trn;
-    r_gaba_a_trn2tcr_temp=r_gaba_a_trn2tcr+(3/32)*k1_r_gaba_a_trn2tcr+(9/32)*k2_r_gaba_a_trn2tcr;
-    r_gaba_a_trn2trn_temp=r_gaba_a_trn2trn+(3/32)*k1_r_gaba_a_trn2trn+(9/32)*k2_r_gaba_a_trn2trn;
-    r_gaba_a_in2tcr_temp=r_gaba_a_in2tcr+(3/32)*k1_r_gaba_a_in2tcr+(9/32)*k2_r_gaba_a_in2tcr;
-    r_gaba_a_in2in_temp=r_gaba_a_in2in+(3/32)*k1_r_gaba_a_in2in+(9/32)*k2_r_gaba_a_in2in;
-    r_gaba_b_temp=r_gaba_b+(3/32)*k1_r_gaba_b+(9/32)*k2_r_gaba_b;
-    G_temp=G+(3/32)*k1_G+(9/32)*k2_G;
-    V_tcr_temp = V_tcr+(3/32)*k1_V_tcr+(9/32)*k2_V_tcr;
-    V_trn_temp = V_trn+(3/32)*k1_V_trn+(9/32)*k2_V_trn;
-    V_in_temp = V_in+(3/32)*k1_V_in+(9/32)*k2_V_in;
+r_ampa_ret2tcr_temp=r_ampa_ret2tcr+(3/32)*k1_r_ampa_ret2tcr+(9/32)*k2_r_ampa_ret2tcr;
+r_ampa_ret2in_temp=r_ampa_ret2in+(3/32)*k1_r_ampa_ret2in+(9/32)*k2_r_ampa_ret2in;
+r_ampa_tcr2trn_temp=r_ampa_tcr2trn+(3/32)*k1_r_ampa_tcr2trn+(9/32)*k2_r_ampa_tcr2trn;
+r_gaba_a_trn2tcr_temp=r_gaba_a_trn2tcr+(3/32)*k1_r_gaba_a_trn2tcr+(9/32)*k2_r_gaba_a_trn2tcr;
+r_gaba_a_trn2trn_temp=r_gaba_a_trn2trn+(3/32)*k1_r_gaba_a_trn2trn+(9/32)*k2_r_gaba_a_trn2trn;
+r_gaba_a_in2tcr_temp=r_gaba_a_in2tcr+(3/32)*k1_r_gaba_a_in2tcr+(9/32)*k2_r_gaba_a_in2tcr;
+r_gaba_a_in2in_temp=r_gaba_a_in2in+(3/32)*k1_r_gaba_a_in2in+(9/32)*k2_r_gaba_a_in2in;
+r_gaba_b_temp=r_gaba_b+(3/32)*k1_r_gaba_b+(9/32)*k2_r_gaba_b;
+G_temp=G+(3/32)*k1_G+(9/32)*k2_G;
+V_tcr_temp = V_tcr+(3/32)*k1_V_tcr+(9/32)*k2_V_tcr;
+V_trn_temp = V_trn+(3/32)*k1_V_trn+(9/32)*k2_V_trn;
+V_in_temp = V_in+(3/32)*k1_V_in+(9/32)*k2_V_in;
 
 
     T_tcr_temp = Tval(1)/(1+exp(-(V_tcr_temp - Tval(3))/Tval(2))); %% transmitter concentration of the TCR synapse
@@ -233,18 +243,18 @@ k4_V_tcr = h * -(Carr(4) * I_ampa_ret2tcr_temp + Carr(2) * I_gaba_a_trn2tcr_temp
 k4_V_trn = h * -(Carr(1) * I_ampa_tcr2trn_temp + Carr(5) * I_gaba_a_trn2trn_temp + I_leak_trn_temp)*(1/Cm);
 k4_V_in =  h * -(Carr(6) * I_ampa_ret2in_temp + Carr(8) * I_gaba_a_in2in_temp + I_leak_in_temp)*(1/Cm); 
 
-    r_ampa_ret2tcr_temp = r_ampa_ret2tcr+(439/216)*k1_r_ampa_ret2tcr-8*k2_r_ampa_ret2tcr+(3680/513)*k3_r_ampa_ret2tcr-(845/4104)*k4_r_ampa_ret2tcr;
-    r_ampa_ret2in_temp = r_ampa_ret2in+(439/216)*k1_r_ampa_ret2in-8*k2_r_ampa_ret2in+(3680/513)*k3_r_ampa_ret2in-(845/4104)*k4_r_ampa_ret2in;
-    r_ampa_tcr2trn_temp = r_ampa_tcr2trn+(439/216)*k1_r_ampa_tcr2trn-8*k2_r_ampa_tcr2trn+(3680/513)*k3_r_ampa_tcr2trn-(845/4104)*k4_r_ampa_tcr2trn;
-    r_gaba_a_trn2tcr_temp = r_gaba_a_trn2tcr+(439/216)*k1_r_gaba_a_trn2tcr-8*k2_r_gaba_a_trn2tcr+(3680/513)*k3_r_gaba_a_trn2tcr-(845/4104)*k4_r_gaba_a_trn2tcr;
-    r_gaba_a_trn2trn_temp = r_gaba_a_trn2trn+(439/216)*k1_r_gaba_a_trn2trn-8*k2_r_gaba_a_trn2trn+(3680/513)*k3_r_gaba_a_trn2trn-(845/4104)*k4_r_gaba_a_trn2trn;
-    r_gaba_a_in2tcr_temp = r_gaba_a_in2tcr+(439/216)*k1_r_gaba_a_in2tcr-8*k2_r_gaba_a_in2tcr+(3680/513)*k3_r_gaba_a_in2tcr-(845/4104)*k4_r_gaba_a_in2tcr;
-    r_gaba_a_in2in_temp = r_gaba_a_in2in+(439/216)*k1_r_gaba_a_in2in-8*k2_r_gaba_a_in2in+(3680/513)*k3_r_gaba_a_in2in-(845/4104)*k4_r_gaba_a_in2in;
-    r_gaba_b_temp = r_gaba_b+(439/216)*k1_r_gaba_b-8*k2_r_gaba_b+(3680/513)*k3_r_gaba_b-(845/4104)*k4_r_gaba_b;
-    G_temp = G+(439/216)*k1_G-8*k2_G+(3680/513)*k3_G-(845/4104)*k4_G;
-    V_tcr_temp=V_tcr+(439/216)*k1_V_tcr-8*k2_V_tcr+(3680/513)*k3_V_tcr-(845/4104)*k4_V_tcr;
-    V_trn_temp=V_trn+(439/216)*k1_V_trn-8*k2_V_trn+(3680/513)*k3_V_trn-(845/4104)*k4_V_trn;
-    V_in_temp=V_in+(439/216)*k1_V_in-8*k2_V_in+(3680/513)*k3_V_in-(845/4104)*k4_V_in;
+r_ampa_ret2tcr_temp = r_ampa_ret2tcr+(439/216)*k1_r_ampa_ret2tcr-8*k2_r_ampa_ret2tcr+(3680/513)*k3_r_ampa_ret2tcr-(845/4104)*k4_r_ampa_ret2tcr;
+r_ampa_ret2in_temp = r_ampa_ret2in+(439/216)*k1_r_ampa_ret2in-8*k2_r_ampa_ret2in+(3680/513)*k3_r_ampa_ret2in-(845/4104)*k4_r_ampa_ret2in;
+r_ampa_tcr2trn_temp = r_ampa_tcr2trn+(439/216)*k1_r_ampa_tcr2trn-8*k2_r_ampa_tcr2trn+(3680/513)*k3_r_ampa_tcr2trn-(845/4104)*k4_r_ampa_tcr2trn;
+r_gaba_a_trn2tcr_temp = r_gaba_a_trn2tcr+(439/216)*k1_r_gaba_a_trn2tcr-8*k2_r_gaba_a_trn2tcr+(3680/513)*k3_r_gaba_a_trn2tcr-(845/4104)*k4_r_gaba_a_trn2tcr;
+r_gaba_a_trn2trn_temp = r_gaba_a_trn2trn+(439/216)*k1_r_gaba_a_trn2trn-8*k2_r_gaba_a_trn2trn+(3680/513)*k3_r_gaba_a_trn2trn-(845/4104)*k4_r_gaba_a_trn2trn;
+r_gaba_a_in2tcr_temp = r_gaba_a_in2tcr+(439/216)*k1_r_gaba_a_in2tcr-8*k2_r_gaba_a_in2tcr+(3680/513)*k3_r_gaba_a_in2tcr-(845/4104)*k4_r_gaba_a_in2tcr;
+r_gaba_a_in2in_temp = r_gaba_a_in2in+(439/216)*k1_r_gaba_a_in2in-8*k2_r_gaba_a_in2in+(3680/513)*k3_r_gaba_a_in2in-(845/4104)*k4_r_gaba_a_in2in;
+r_gaba_b_temp = r_gaba_b+(439/216)*k1_r_gaba_b-8*k2_r_gaba_b+(3680/513)*k3_r_gaba_b-(845/4104)*k4_r_gaba_b;
+G_temp = G+(439/216)*k1_G-8*k2_G+(3680/513)*k3_G-(845/4104)*k4_G;
+V_tcr_temp=V_tcr+(439/216)*k1_V_tcr-8*k2_V_tcr+(3680/513)*k3_V_tcr-(845/4104)*k4_V_tcr;
+V_trn_temp=V_trn+(439/216)*k1_V_trn-8*k2_V_trn+(3680/513)*k3_V_trn-(845/4104)*k4_V_trn;
+V_in_temp=V_in+(439/216)*k1_V_in-8*k2_V_in+(3680/513)*k3_V_in-(845/4104)*k4_V_in;
 
     T_tcr_temp = Tval(1)/(1+exp(-(V_tcr_temp - Tval(3))/Tval(2))); %% transmitter concentration of the TCR synapse
     T_trn_temp = Tval(1)/(1+exp(-(V_trn_temp - Tval(3))/Tval(2))); %% transmitter concentration of the TRN synapse
@@ -277,7 +287,7 @@ k5_V_tcr = h * -(Carr(4) * I_ampa_ret2tcr_temp + Carr(2) * I_gaba_a_trn2tcr_temp
 k5_V_trn = h * -(Carr(1) * I_ampa_tcr2trn_temp + Carr(5) * I_gaba_a_trn2trn_temp + I_leak_trn_temp)*(1/Cm);
 k5_V_in =  h * -(Carr(6) * I_ampa_ret2in_temp + Carr(8) * I_gaba_a_in2in_temp + I_leak_in_temp)*(1/Cm); 
 
-
+%ORDER 4
 
       yvect(1,i+1)=yvect(1,i)+(25/216)*k1_r_ampa_ret2tcr+(1408/2565)*k3_r_ampa_ret2tcr+(2197/4101)*k4_r_ampa_ret2tcr-(1/5)*k5_r_ampa_ret2tcr;
       yvect(2,i+1)=yvect(2,i)+(25/216)*k1_r_ampa_ret2in+(1408/2565)*k3_r_ampa_ret2in+(2197/4101)*k4_r_ampa_ret2in-(1/5)*k5_r_ampa_ret2in;
@@ -314,5 +324,4 @@ k5_V_in =  h * -(Carr(6) * I_ampa_ret2in_temp + Carr(8) * I_gaba_a_in2in_temp + 
 end 
 Y=yvect;
 T=[T_tcrArr; T_inArr; T_trnArr];
-  
-    
+% end   
